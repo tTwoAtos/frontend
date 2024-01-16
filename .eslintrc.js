859ba7c1f5db6f45c08bc5f9ddfefe14593c4dbb
hamzaRebb <s35394@stagiaire.aelion.fr>
2024-01-16 14:38:44 +0100

    config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["plugin:vue/vue3-essential", "plugin:prettier/recommended"],
+    overrides: [
+        {
+            files: ["*.ts", "*.tsx"],
+            parser: "@typescript-eslint/parser",
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+                project: "./tsconfig.json",
+            },
+            extends: ["plugin:@typescript-eslint/recommended"],
+            rules: {
+                "@typescript-eslint/dot-notation": "off",
+            }
+        },
+    ],
+    plugins: ["vue", "@typescript-eslint", "prettier"],
+    rules: {
+        "vue/no-multiple-template-root": "off",
+        "vue/require-default-prop": "off",
+        "vue/require-prop-types": "off",
+        "comma-dangle": ["error", "always-multiline"],
+        "vue/script-indent": ["error", 2, { baseIndent: 1 }],
+        "vue/html-indent": ["error", 2, { baseIndent: 1 }],
+        "vue/html-self-closing": [
+            "error",
+            {
+                html: {
+                    void: "never",
+                    normal: "never",
+                    component: "always",
+                }
+            },
+        ],
+        "vue/multi-word-component-names": "off",
+    }
+};
