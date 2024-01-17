8181f2a98454b53463049d7c7c29007be6e2df0b
hamzaRebb <s35394@stagiaire.aelion.fr>
2024-01-17 14:48:45 +0100

    Added a first jet using json and implemented stock update functionality in StockProduct-Update.vue

---
+++ src/components/StockProduct-Update.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>Update Stock</h2>
+    <div v-for="(item, index) in products" :key="index">
+      <h4>{{ item.name }}</h4>
+      <label for="stock">Stock: </label>
+      <input type="number" id="stock" v-model="item.stock" @change="updateStock(index)">
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import products from '@/assets/products.json'
+
+export default {
+  data() {
+    return {
+      products: products
+    }
+  },
+  methods: {
+    updateStock(index: number) {
+      (this as any).$set(this.products, index, { ...this.products[index] });
+    }
+  }
+}
+</script>
