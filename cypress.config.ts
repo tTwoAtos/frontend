f5001d99c9d8ad1b991a0fe73ee8ccaceb902cd2
hamzaRebb <s35394@stagiaire.aelion.fr>
2024-01-18 14:21:56 +0100

    Refactor codebase: update component declarations, enhance stock update functionality, modify GitHub workflows, and add Cypress project ID

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+    projectId: "bgqso6",
     e2e: {
         setupNodeEvents() {
             // implement node event listeners here
--- src/components/StockProduct-Update.vue
+++ src/components/StockProduct-Update.vue
@@ -4,7 +4,7 @@
     <div v-for="(item, index) in products" :key="index" data-cy="product">
       <h4>{{ item.name }}</h4>
       <label for="stock">Stock: </label>
-      <input type="number" id="stock" v-model="item.stock" @change="updateStock(index)" data-cy="stock-input">
+      <input type="number" id="stock" v-model="item.stock" :disabled="item.stock === 0" @change="updateStock(index)" data-cy="stock-input">
     </div>
   </div>
 </template>
@@ -13,18 +13,32 @@
 import products from '@/assets/products.json';
 
 export default {
+  /**
+   * Initializes the data for the component.
+   *
+   * @return {Object} - The initial data object.
+   */
   data() {
     return {
       products: JSON.parse(localStorage.getItem('products') as string) || products,
+      debouncedUpdate: null as any,
     };
   },
   methods: {
+    /**
+     * Updates the stock of a product at the specified index.
+     *
+     * @param {number} index - The index of the product in the products array.
+     * @return {void} This function does not return a value.
+     */
     updateStock(index: number) {
       const product = this.products[index];
       product.stock = parseInt(product.stock);
 
-      // Save the updated products array to local storage
-      localStorage.setItem('products', JSON.stringify(this.products));
+      clearTimeout(this.debouncedUpdate);
+      this.debouncedUpdate = setTimeout(() => {
+        localStorage.setItem('products', JSON.stringify(this.products));
+      }, 500)
     },
   },
 };
